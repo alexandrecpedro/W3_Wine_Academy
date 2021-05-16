b282be14a2b88f18f10edf135cc6f4e4eaceeda1
Martincho <martinembon@hotmail.com>
2021-05-16 11:27:06 -0300

    actualizacion cursos, sobre e nav

---
+++ assets/js/sobre.js
@@ -0,0 +1,17 @@
+const participantes =  document.querySelector('.read-more-container');
+
+participantes.addEventListener('click', e =>{
+
+    const current = e.target;
+
+    const isReadMoreBtn = current.className.includes('read-more-btn');
+
+    if(!isReadMoreBtn) return;
+
+    const currentText = e.target.parentNode.querySelector('.read-more-text');
+
+    currentText.classList.toggle('read-more-text--show');
+
+    current.textContent = current.textContent.includes('Read More') ? "Saiba menos..." : "Saiba mas...";
+
+})
