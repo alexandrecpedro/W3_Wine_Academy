f7c16c4cd17c022402656dd54f2cc1496b33b9a4
Alexandre Pedro <alexandrecpedro@gmail.com>
2021-06-04 23:31:49 -0300

    footer atual

--- README.md
+++
@@ -1,1 +0,0 @@
-Teste de github na branch develop
--- controllers/institucionalController.js
+++ controllers/institucionalController.js
@@ -3,10 +3,11 @@ const institucionalController = {
         return response.render('index', { titulo: 'Home' });
     },
     sobre: (request, response) => {
-        return response.render('sobre', { titulo: 'Sobre'});
+        return response.render('sobre', { titulo: 'Sobre' });
     },
     servicos: (request, response) => {
         /** renderiza a view Serviços e passa titulo e lista de serviços cadastrados */
         return response.render('servicos', { titulo: 'Serviços', servicos });
     },
-    contato: (request, response) => {
+    contato: (request, response) => {}
+}
