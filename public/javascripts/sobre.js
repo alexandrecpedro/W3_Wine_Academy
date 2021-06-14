fffc66bc6f1d85d4deb0f3162018af20666f1fad
Martincho <martinembon@hotmail.com>
2021-06-14 12:58:32 -0300

    atualizacao para pull

--- public/javascripts/sobre.js
+++ public/javascripts/sobre.js
@@ -1,17 +1,11 @@
 const participantes =  document.querySelector('.read-more-container');
 
 participantes.addEventListener('click', e =>{
-
     const current = e.target;
-
     const isReadMoreBtn = current.className.includes('mostrar');
-
     if(!isReadMoreBtn) return;
-
     const currentText = e.target.parentNode.querySelector('.mostrar-mas');
-
     currentText.classList.toggle('mostrar-mas--show');
-
     current.textContent = current.textContent.includes('Mostrar mais') ? "Mostrar menos..." : "Mostrar mais";
 
 })
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const institucionalController = require("../controllers/institucionalController")
+const { check } = require("express-validator")
+const validateRegisterLogin = require("../middlewares/validacao/login")
 
 /* GET home page. */
 router.get('/', institucionalController.index)
 router.get('/cursos', institucionalController.cursos)
 router.get('/planos', institucionalController.planos)
 router.get('/sobre', institucionalController.sobre)
-router.get('/login', institucionalController.login)
+router.get('/login', validateRegisterLogin, institucionalController.login)
 
 
 
