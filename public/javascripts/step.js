87d7fa418242372e8e59b8787e74acee71388f2c
Martincho <martinembon@hotmail.com>
2021-06-14 15:09:38 -0300

    atualizacao 14-06

---
+++ public/javascripts/step.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("stest")
+const pass = document.getElementById("password")
+const confPass = document.getElementById("confirm_password")
+const errorElement = document.getElementById("err")
+const btn = document.getElementById("submit")
+
