cb0158ac25adc28dee33e8d5091f42b790c06fbe
Martincho <martinembon@hotmail.com>
2021-06-14 15:59:52 -0300

    password match val

--- public/javascripts/step.js
+++ public/javascripts/step.js
@@ -1,6 +1,14 @@
-const form = document.getElementById("stest")
+const form = document.getElementById("st1")
 const pass = document.getElementById("password")
-const confPass = document.getElementById("confirm_password")
+const confPass = document.getElementById("confirm")
 const errorElement = document.getElementById("err")
-const btn = document.getElementById("submit")
+const btn = document.getElementById("btn1")
 
+form.addEventListener("submit",(e)=>{
+    if(pass.value!=confPass.value){
+        e.preventDefault()
+    } if (pass.value==confPass.value){
+        e.returnValue = true;
+    }
+    
+})
